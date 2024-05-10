c26f56dbd5ad7bf92558d556d73fb3ca37d30f7b
Erick gabriel <aazhos@gmail.com>
2024-05-09 22:22:38 -0300

    first commit

---
+++ soma.js
@@ -0,0 +1,10 @@
+function somarValores(){
+    var s1 = document.getElementById("s1").value;
+    var s2 = document.getElementById("s2").value;
+    var s3 = parseInt(s1) + parseInt(s2);
+    alert(s3);}
+function divisaoValores(){
+        var s3 = document.getElementById("s3").value;
+        var s4 = document.getElementById("s4").value;
+        var s5 = parseInt(s3) - parseInt(s4);
+        alert(s5);}
